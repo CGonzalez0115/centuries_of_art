a66c43b7bb5a2ffe51ef052512cde4bc033622eb
Christina Gonzalez <cmgamway@gmail.com>
2019-10-04 04:15:07 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in centuries_of_art.gemspec
+gemspec
---
+++ lib/centuries_of_art.rb
@@ -0,0 +1,6 @@
+require "centuries_of_art/version"
+
+module CenturiesOfArt
+  class Error < StandardError; end
+  # Your code goes here...
+end
